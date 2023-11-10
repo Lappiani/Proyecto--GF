b654a182cc6bc4e933c2c5d068c6d27a2efcbabb
Lappiani <141185089+Lappiani@users.noreply.github.com>
2023-11-10 11:55:57 -0300

    Prueba shiny

--- Aplicacion/app.R
+++ Aplicacion/app.R
@@ -2,19 +2,26 @@
 # Load necessary libraries
 library(shiny)
 library(quantmod)
+library(ggplot2)
+library(dplyr)
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("Resumen Proyecto Grupo 13"),
-  h2("Note:"),
-  p("This is a simple Shiny app for plotting stock prices. Please enter a valid stock symbol and click 'Plot'."),
+  titlePanel("Stock Price App"),
+  h3("Note:"),
+  p("This is a simple Shiny app for plotting stock prices and options strike prices. Please enter a valid stock symbol and click 'Plot'."),
   sidebarLayout(
     sidebarPanel(
       textInput("symbol", "Enter Stock Symbol:", value = "AAPL"),
+      dateRangeInput("dates", "Date range:", start = Sys.Date() - 30, end = Sys.Date()),
       actionButton("plot", "Plot")
     ),
     mainPanel(
-      plotOutput("pricePlot")
+      tabsetPanel(
+        tabPanel("Price", plotOutput("pricePlot")),
+        tabPanel("Volume", plotOutput("volumePlot")),
+        tabPanel("Options", plotOutput("optionsPlot"))
+      )
     )
   )
 )
@@ -23,10 +30,26 @@ ui <- fluidPage(
 server <- function(input, output) {
   observeEvent(input$plot, {
     symbol <- input$symbol
-    data <- getSymbols(symbol, src = "yahoo", auto.assign = FALSE)
+    start_date <- input$dates[1]
+    end_date <- input$dates[2]
+    data <- getSymbols(symbol, src = "yahoo", from = start_date, to = end_date, auto.assign = FALSE)
     output$pricePlot <- renderPlot({
       chartSeries(data, theme = chartTheme("white"), name = paste("Stock Prices for", symbol))
       title(ylab = "Price (in market currency)")
+    })
+    output$volumePlot <- renderPlot({
+      barChart(data, theme = chartTheme("white"), name = paste("Trading Volume for", symbol))
+      title(ylab = "Volume")
+    })
+    output$optionsPlot <- renderPlot({
+      options_data <- getOptionChain(symbol)
+      calls <- options_data$calls
+      puts <- options_data$puts
+      options_df <- rbind(calls, puts)
+      options_df %>%
+        ggplot(aes(x = Strike, y = Last)) +
+        geom_point() +
+        labs(title = paste("Options Strike Prices for", symbol), x = "Strike Price", y = "Last Price")
     })
   })
 }
